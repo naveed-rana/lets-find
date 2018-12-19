2f13f9c579fba31fac8b5d54e28f967f789e5a7b
naveed-rana <rana.naveed812@gmail.com>
2018-12-19 14:38:31 +0500

    backend server setup

--- Backend/static/js/app.js
+++
@@ -1,9 +0,0 @@
-  $(document).ready(function(){
-    $('.sidenav').sidenav(); 
-    $('.slider').slider({duration:1000,interval:5500});
-    $('.collapsible').collapsible();
-    $(".dropdown-trigger").dropdown({coverTrigger:false});
-    $('.modal').modal();
-    
-  });
-      
