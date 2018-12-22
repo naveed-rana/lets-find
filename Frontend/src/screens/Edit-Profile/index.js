c5235aadbaf5051ea6862a1d1d19cf6f93c1c7be
FayyazGM <fayyazjee11@gmail.com>
2018-12-22 14:15:44 +0500

    Edit Profile Screen Full By Full Done

--- Frontend/src/screens/Edit-Profile/index.js
+++ Frontend/src/screens/Edit-Profile/index.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-import {  ImageBackground, Image } from 'react-native';
-import {  Text, Content, Item, Input, Form, Icon, View, Button, CheckBox } from 'native-base';
+import { ImageBackground, Image, StatusBar } from 'react-native';
+import { Text, Content, Item, Input, Form, Icon, View, Button, Label , Textarea} from 'native-base';
 import styles from './style';
 
 export default class SignUpScreen extends Component {
     render() {
-      return (
-          <Content style={styles.wrapper}>
+        return (
+            <Content style={styles.wrapper}>
+                <View>
+                    <StatusBar
+                        backgroundColor="#05CE1D"
+                        barStyle="light-content"
+                    />
+                </View>
                 <View style={styles.topcontent}>
                     <Icon type='AntDesign' name='close' style={styles.topCross} />
                     <Text style={styles.topText}> Edit Profile </Text>
                     <Text style={styles.topsave}> Save </Text>
                 </View>
-          </Content>
-  
-      );
+                <View style={styles.profileImageContainer}>
+                    <Image source={require('../../media/Edit-profile-top-image.png')} />
+                </View>
+                {/* Form Wala Part starts */}
+                <Form style={styles.formStyle}>
+                    <View style={styles.flContainer}>
+                        <Item stackedLabel style={styles.firstNameWrapper} regular>
+                            <Label style={styles.label}>First Name:-</Label>
+                            <Input style={styles.firsNameStyle} placeholder="Fayyaz" placeholderTextColor="#808080" />
+                        </Item>
+                        <Item stackedLabel style={styles.firstNameWrapper} regular>
+                            <Label style={styles.label}>Last Name:-</Label>
+                            <Input style={styles.firsNameStyle} placeholder="Ghulam Muhammad" placeholderTextColor="#808080" />
+                        </Item>
+                    </View>
+                    <Item stackedLabel style={styles.phonenumberWrapper} regular>
+                        <Label style={styles.label}>Phone Number:-</Label>
+                        <Input style={styles.firsNameStyle} placeholder="+92 324 7636236" placeholderTextColor="#808080" />
+                    </Item>
+                    <Item stackedLabel style={styles.phonenumberWrapper} regular>
+                        <Label style={styles.label}>Description:-</Label>
+                        {/* <Input style={styles.firsNameStyle} placeholder="My Name Is Fayyaz . This is my detail" placeholderTextColor="#808080" /> */}
+                        <Textarea rowSpan={5} placeholder="My Name is Fayyaz . This is my detail" style={styles.detailArea} />
+                    </Item>
+                    <Item stackedLabel style={styles.phonenumberWrapper} regular>
+                        <Label style={styles.label}>Country:-</Label>
+                        <Input style={styles.firsNameStyle} placeholder="Pakistan" placeholderTextColor="#808080" />
+                    </Item>
+                    <Button full regular style={{ marginVertical: 10,marginHorizontal: 2 , backgroundColor: '#05CE1D',borderRadius:4 }}>
+                        <Text style={{ color: '#fff', fontWeight: 'bold' }}>Save & Update</Text>
+                    </Button>
+                </Form>
+            </Content>
+
+        );
     }
-  }
+}
--- Frontend/src/screens/Edit-Profile/style.js
+++ Frontend/src/screens/Edit-Profile/style.js
@@ -22,4 +22,49 @@ export default styles = StyleSheet.create({
     topsave:{
     color:"#fff"
     },
+    profileImageContainer:{
+        alignItems: 'center',
+        marginTop:25
+    },
+    formStyle:{
+        marginTop:25,
+        marginLeft:5,
+        marginRight:5
+    },
+    flContainer:{
+        justifyContent: "space-between",
+        flexDirection:"row",
+    },
+    firstNameWrapper:{
+        width: '49%',
+        borderWidth:5,
+        borderColor:"#E6E6E6",
+        padding:3,
+        borderRadius:4
+    },
+    lastNameWrapper:{
+        width: '49%',
+    },
+    firsNameStyle:{
+
+    },
+    label:{
+        marginLeft:7,
+        marginBottom:-7,
+        color:"#9B9B9B"
+    },
+    phonenumberWrapper:{
+        width: '99%',
+        borderWidth:5,
+        borderColor:"#E6E6E6",
+        padding:3,
+        borderRadius:4,
+        marginLeft:2,
+        marginRight:2,
+        marginTop:5
+    },
+    detailArea:{
+        width:"100%",
+        marginTop:5
+    }
   })
