3dc75c1c55f663d6b8731cf3d2fc2298d8e496d1
naveed-rana <rana.naveed812@gmail.com>
2018-12-23 17:00:37 +0500

    complete setup

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,10 +1,5 @@
-
 import React, {Component} from 'react';
-import { View} from 'react-native';
-import SplashScreen from './src/screens/SplashScreen';
-import Home from './src/screens/HomeScreen';
-import Login from './src/screens/signup';
-import {Content, Container} from 'native-base';
+import { Container} from 'native-base';
 import HomeScreen from './src/screens/Home'
 
 
@@ -27,15 +22,6 @@ export default class App extends Component {
   render() {
     const { SplashScreens } = this.state;
     return (
-      // <Content>    
-      //    {SplashScreens ?
-
-      //     <SplashScreen />
-      //     :
-
-      //     <Login />
-      //       }
-      // </Content>
 
       <Container>
         <HomeScreen />
--- Frontend/src/screens/searchScreen/index.js
+++ Frontend/src/screens/searchScreen/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet, Image, StatusBar } from 'react-native';
+import { Image, StatusBar } from 'react-native';
 import { View, Text, Content, Item, Input, Icon, Card, CardItem, Body } from 'native-base';
+import styles from './style';
+
 
 export default class componentName extends Component {
     constructor(props) {
@@ -271,61 +273,3 @@ export default class componentName extends Component {
     }
 }
 
-const styles = StyleSheet.create({
-    searchContainer: {
-        backgroundColor: '#05CE1D',
-        paddingTop: 20,
-        paddingBottom: 10
-    },
-    searchInput: {
-        color: '#f5f5f5',
-        backgroundColor: '#fff',
-        flexDirection: 'row',
-        marginHorizontal: 10,
-        paddingHorizontal: 10,
-        borderRadius: 5,
-    },
-    camIcon: {
-        marginTop: 10
-    },
-    itemStyle: {
-        borderColor: 'transparent'
-    },
-    filterContainer: {
-        marginLeft: 15,
-        marginTop: 10,
-        flexDirection: 'row',
-        flex: 1
-    },
-    filterImage: {
-        width: 90,
-        height: 90,
-        borderRadius: 50
-    },
-    cardContainer: {
-        marginHorizontal: 10
-    },
-    cardInnerContainer: {
-        flexDirection: 'row',
-    },
-    cardHeader: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    statusText: {
-        color: '#05CE1D',
-    },
-    nameText: {
-        fontSize: 10
-    },
-    textContainer: {
-        flex: 1,
-        marginLeft: 10
-    },
-    readMore: {
-        color: '#0366d6',
-        fontSize: 12,
-        paddingTop: 2
-    }
-})
