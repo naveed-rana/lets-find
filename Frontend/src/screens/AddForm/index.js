f2911f9374914ed48d893139bd450646229b27eb
Asif-chohan <rmasifchohan@gmail.com>
2018-12-25 16:26:17 +0500

    add person screen modified

--- Frontend/src/screens/AddForm/index.js
+++ Frontend/src/screens/AddForm/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { StatusBar } from 'react-native'
+import React, { Component } from "react";
+import { StatusBar } from "react-native";
 import {
   View,
   Text,
@@ -11,10 +11,15 @@ import {
   Right,
   Item,
   Input,
-  Textarea
-} from 'native-base';
+  Textarea,
+  Header,
+  Icon,
+  Title,
+  Body,
+  Picker
+} from "native-base";
 
-import styles from './style';
+import styles from "./style";
 
 export default class AddForm extends Component {
   constructor(props) {
@@ -26,87 +31,154 @@ export default class AddForm extends Component {
       location: "",
       description: "",
       image: "",
+      status: true,
+      selected: "key1"
     };
   }
+  onValueChange(value) {
+    this.setState({
+      selected: value
+    });
+  }
 
-  onSubmit= ()=>{
-    console.log('====================================');
+  hadleStatus = () => {
+    this.setState(preState => {
+      return {
+        status: !preState.status
+      };
+    });
+  };
+
+  onSubmit = () => {
+    console.log("====================================");
     console.log(this.state.name);
     console.log(this.state.gender);
     console.log(this.state.age);
     console.log(this.state.description);
     console.log(this.state.location);
-    console.log('====================================');
-  }
-  
+    console.log("====================================");
+  };
+
   render() {
     return (
       <Container>
-        <StatusBar
-          backgroundColor="#05ce1d"
-          barStyle="light-content"
-        />
+        <StatusBar backgroundColor="#05CE5D" barStyle="light-content" />
+        <View>
+          <View style={styles.header}>
+            <Icon
+              style={{ fontSize: 30, color: "white" }}
+              type="MaterialCommunityIcons"
+              name="keyboard-backspace"
+            />
+          </View>
+        </View>
         <Content>
-
-          <View style={{ alignItems: 'center' }}>
-            <View style={styles.topImagecircle}>
-              <Thumbnail style={styles.topImageStyle} source={require('../../media/Top-image.png')} />
-            </View>
-
+          <View style={{ alignItems: "center" }}>
             <View style={styles.btnViewStyle}>
               <Left>
-                <Button success rounded>
-                  <Text>Missing</Text>
+                <Button bordered={!this.state.status} success rounded>
+                  <Text style={styles.tab} onPress={this.hadleStatus}>
+                    Missing
+                  </Text>
                 </Button>
               </Left>
               <Right>
-                <Button bordered success rounded>
-                  <Text style={{ fontWeight: 'bold' }}>Found</Text>
+                <Button bordered={this.state.status} success rounded>
+                  <Text style={styles.tab} onPress={this.hadleStatus}>
+                    Found
+                  </Text>
                 </Button>
               </Right>
             </View>
             <View style={styles.inputViewStyle}>
               <Item rounded style={styles.inputStyle}>
-                <Input onChangeText={event => this.setState({
-                  name: event
-                })} placeholder='Name' style={styles.inputTextSize} />
+                <Input
+                  onChangeText={event =>
+                    this.setState({
+                      name: event
+                    })
+                  }
+                  placeholder="Name"
+                  style={styles.inputTextSize}
+                />
               </Item>
               <Item rounded style={styles.inputStyle}>
-                <Input onChangeText={event => this.setState({
-                  age: event
-                })} placeholder='Age Group' style={styles.inputTextSize} />
+                {/* <Input
+                  onChangeText={event =>
+                    this.setState({
+                      age: event
+                    })
+                  }
+                  placeholder="Age Group"
+                  style={styles.inputTextSize}
+                /> */}
+                <Picker
+                  mode="dropdown"
+                  style={{ borderRadius: 20, paddingRight: 10 }}
+                  headerStyle={{ backgroundColor: "#b95dd3",}}
+                  headerBackButtonTextStyle={{ color: "#fff" }}
+                  headerTitleStyle={{ color: "#fff" }}
+                  selectedValue={this.state.selected}
+                  onValueChange={this.onValueChange.bind(this)}
+                >
+                  <Picker.Item
+                    style={styles.picItem}
+                    label="Wallet"
+                    value="key0"
+                  />
+                  <Picker.Item label="ATM Card" value="key1" />
+                  <Picker.Item label="Debit Card" value="key2" />
+                  <Picker.Item label="Credit Card" value="key3" />
+                  <Picker.Item label="Net Banking" value="key4" />
+                </Picker>
               </Item>
               <Item rounded style={styles.inputStyle}>
-                <Input 
-                onChangeText={event => this.setState({
-                  gender: event
-                })} placeholder='Gender' style={styles.inputTextSize} />
+                <Input
+                  onChangeText={event =>
+                    this.setState({
+                      gender: event
+                    })
+                  }
+                  placeholder="Gender"
+                  style={styles.inputTextSize}
+                />
               </Item>
               <Item rounded style={styles.inputStyle}>
-                <Input 
-                onChangeText={event => this.setState({
-                  location: event
-                })} placeholder='Location' style={styles.inputTextSize} />
+                <Input
+                  onChangeText={event =>
+                    this.setState({
+                      location: event
+                    })
+                  }
+                  placeholder="Location"
+                  style={styles.inputTextSize}
+                />
               </Item>
               <Item rounded style={styles.inputStyle}>
-                <Textarea onChangeText={event => this.setState({
-                  description: event
-                })} style={styles.txtareaStyle} placeholder="Description" />
+                <Textarea
+                  onChangeText={event =>
+                    this.setState({
+                      description: event
+                    })
+                  }
+                  style={styles.txtareaStyle}
+                  placeholder="Description"
+                />
               </Item>
             </View>
           </View>
           <Text style={styles.uploadTextStyle}>Upload Photo</Text>
           <View style={styles.bottomStyle}>
-            <Thumbnail style={styles.bottomImageStyle} source={require('../../media/upload-photo.png')} />
-            <Button full rounded success 
-            onPress={this.onSubmit}>
+            <Thumbnail
+              style={styles.bottomImageStyle}
+              source={require("../../media/upload-photo.png")}
+            />
+            <Button full rounded success onPress={this.onSubmit}>
               <Text>Submit & Post</Text>
             </Button>
           </View>
         </Content>
-
       </Container>
     );
   }
 }
-
--- Frontend/src/screens/AddForm/style.js
+++ Frontend/src/screens/AddForm/style.js
@@ -1,52 +1,68 @@
-export default styles = {
-    inputTextSize: {
-      fontSize: 15
-    },
-    bottomImageStyle: {
-      width: 147,
-      height: 143,
-      marginBottom: 20
-    },
-    bottomStyle: {
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      alignItems: 'center'
-    },
-    uploadTextStyle: {
-      textAlign: 'left',
-      fontSize: 14,
-      marginLeft: 50
-    },
-    txtareaStyle: {
-      width: '100%',
-      height: 150
-    },
-    inputViewStyle: {
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      alignItems: 'center'
-    },
-    btnViewStyle: {
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      flex: 1,
-      flexDirection: 'row'
-    },
-    topImageStyle: {
-      width: 100,
-      height: 100
-    },
-    inputStyle: {
-      backgroundColor: '#eff4fb',
-      paddingLeft: 10,
-      marginBottom: 8,
-    },
-    topImagecircle: {
-      marginTop: 20,
-      width: 140,
-      height: 140,
-      padding: 20,
-      borderRadius: 140 / 2,
-      backgroundColor: '#05ce1d'
-    }
+export default (styles = {
+  inputTextSize: {
+    fontSize: 15
+  },
+  bottomImageStyle: {
+    width: 147,
+    height: 143,
+    marginBottom: 20
+  },
+  bottomStyle: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    alignItems: "center"
+  },
+  uploadTextStyle: {
+    textAlign: "left",
+    fontSize: 14,
+    marginLeft: 50
+  },
+  txtareaStyle: {
+    width: "100%",
+    height: 150
+  },
+  inputViewStyle: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    alignItems: "center"
+  },
+  btnViewStyle: {
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    flex: 1,
+    flexDirection: "row",
+
+  },
+  topImageStyle: {
+    width: 100,
+    height: 100
+  },
+  inputStyle: {
+    backgroundColor: "#eff4fb",
+    paddingLeft: 10,
+    marginBottom: 8
+  },
+  topImagecircle: {
+    marginTop: 20,
+    width: 140,
+    height: 140,
+    padding: 20,
+    borderRadius: 140 / 2,
+    backgroundColor: "#05ce1d"
+  },
+  header: {
+    backgroundColor: "#05CE1D",
+    paddingVertical: 12,
+    paddingHorizontal: 10
+  },
+  tab: {
+    fontWeight: "bold",
+    height: "100%",
+    alignSelf: 'center',
+    paddingTop: 7
+  },
+  picItem: {
+    borderBottom: "1px solid gray",
+    borderRadius: 5
   }
+});
