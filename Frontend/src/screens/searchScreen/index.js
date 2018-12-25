dd460ef813bc39e1307ea8482431f96cec3c4fb3
FayyazGM <fayyazjee11@gmail.com>
2018-12-25 05:10:14 +0500

    search screen top bar fixed

--- Frontend/src/screens/searchScreen/index.js
+++ Frontend/src/screens/searchScreen/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Image, StatusBar } from 'react-native';
-import { View, Text, Content, Item, Input, Icon, Card, CardItem, Body } from 'native-base';
+import { Image, StatusBar,ScrollView } from 'react-native';
+import { View, Text, Content, Item, Input, Icon, Card, CardItem, Body, Container } from 'native-base';
 import styles from './style';
 
 
@@ -14,7 +14,7 @@ export default class componentName extends Component {
     render() {
         return (
 
-            <Content>
+            <Container>
 
                 <View>
                     <StatusBar
@@ -31,12 +31,11 @@ export default class componentName extends Component {
                             <Icon style={styles.camIcon} type="Entypo" name="camera" />
                         </View>
                     </Item>
-
                     <View style={styles.filterContainer}>
                         <Image source={require('../../media/Filters.png')} />
                     </View>
                 </View>
-
+                <ScrollView>
                 <View style={styles.cardContainer}>
                     <Card>
                         <CardItem>
@@ -262,11 +261,121 @@ export default class componentName extends Component {
 
                     </Card>
                 </View>
+                {/* start card 2 */}
+
+   <View style={styles.cardContainer}>
+                    <Card>
+                        <CardItem listItemPadding={0}>
+                            <Body>
+                                <View style={styles.cardInnerContainer}>
+                                    <View>
+                                        <Image style={styles.filterImage} source={require('../../media/sham.jpg')} />
+                                    </View>
+
+                                    <View style={styles.textContainer}>
+                                        <View style={styles.cardHeader}>
+                                            <Text >
+                                                Isra Adil
+                                            </Text>
+
+                                            <Text style={styles.statusText}>
+                                                *Missing*
+                                             </Text>
+                                        </View>
+
+                                        <View>
+                                            <Text style={styles.nameText}>
+                                                Posted By Naveed
+                                       </Text>
+                                        </View>
+                                       
+                                        <View style={{flexDirection:'row',paddingTop:5}}>    
+
+                                            <Icon style={{marginLeft:-5}} type="EvilIcons" name="location"/>    
+                                            <Text style={{fontSize:13}}>
+                                                Faisalabad
+                                            </Text>
+
+                                        </View>
+
+                                        <View style={styles.cardHeader}>
+                                            <Text style={styles.readMore}>
+                                                Read More
+                                            </Text>
+
+                                            <Icon style={{ marginTop: -5 }} type="Feather" name="more-horizontal" />
+                                        </View>
+
+                                    </View>
+
+                                </View>
+                            </Body>
+                        </CardItem>
+
+                    </Card>
+                </View>
+
+ {/* end card 2 */}
+ {/* start card 2 */}
+
+ <View style={styles.cardContainer}>
+                    <Card>
+                        <CardItem listItemPadding={0}>
+                            <Body>
+                                <View style={styles.cardInnerContainer}>
+                                    <View>
+                                        <Image style={styles.filterImage} source={require('../../media/sham.jpg')} />
+                                    </View>
+
+                                    <View style={styles.textContainer}>
+                                        <View style={styles.cardHeader}>
+                                            <Text >
+                                                Isra Adil
+                                            </Text>
+
+                                            <Text style={styles.statusText}>
+                                                *Missing*
+                                             </Text>
+                                        </View>
+
+                                        <View>
+                                            <Text style={styles.nameText}>
+                                                Posted By Naveed
+                                       </Text>
+                                        </View>
+                                       
+                                        <View style={{flexDirection:'row',paddingTop:5}}>    
+
+                                            <Icon style={{marginLeft:-5}} type="EvilIcons" name="location"/>    
+                                            <Text style={{fontSize:13}}>
+                                                Faisalabad
+                                            </Text>
+
+                                        </View>
+
+                                        <View style={styles.cardHeader}>
+                                            <Text style={styles.readMore}>
+                                                Read More
+                                            </Text>
+
+                                            <Icon style={{ marginTop: -5 }} type="Feather" name="more-horizontal" />
+                                        </View>
+
+                                    </View>
+
+                                </View>
+                            </Body>
+                        </CardItem>
+
+                    </Card>
+                </View>
+
+ {/* end card 2 */}
+                </ScrollView>
 
  {/* end card 2 */}
 
-
-            </Content>
+            </Container>
 
 
         );
