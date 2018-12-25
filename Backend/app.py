f64c957c95d198e9e8791db9d07e2888b0e9be0c
naveed-rana <rana.naveed812@gmail.com>
2018-12-25 12:31:57 +0500

    flask server setup and database connectivity

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for, jsonify, session, json, session
+from flask_pymongo import PyMongo
 
-app = Flask(__name__)
+from bson.json_util import dumps
+from bson import json_util, ObjectId
+
+#Flask App Structure
+app = Flask(__name__, static_folder="./dist", template_folder="./")
+app.config.from_object(__name__)
+
+#Database connection
+app.config['MONGO_DBNAME'] = 'letsfind'
+app.config['MONGO_URI'] = 'mongodb://NaveedRana:naveed1214@ds117423.mlab.com:17423/letsfind'
+mongo = PyMongo(app)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
 
 #server route
 @app.route('/')
