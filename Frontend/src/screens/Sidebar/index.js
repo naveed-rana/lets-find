416dd80a360424f9d040d6dfdb69728c71e6644e
FayyazGM <fayyazjee11@gmail.com>
2018-12-26 11:56:23 +0500

    Home Page Drawer done But links that i used are dummy links

--- Frontend/src/screens/Sidebar/index.js
+++ Frontend/src/screens/Sidebar/index.js
@@ -1,15 +1,103 @@
 import React, { Component } from 'react';
 import {
   Text,
+  Image
 } from 'react-native';
-import {Content} from 'native-base';
-
+import { Container, Header, Content, List, ListItem, Icon, Left, Body, Right, Switch,Button } from 'native-base';
+import { styles } from './style';
 export default class Sidebar extends Component {
   render() {
     return (
-          <Content style={{backgroundColor:'#FFFFFF'}}>
-            <Text>Drawer</Text>
-          </Content>
+      <Content style={styles.sidebarWrapper}>
+        <Content style={styles.sideBarTopPanel}>
+          <Image source={require("../../media/show-profile.png")} style={{ alignSelf: "center" }} />
+          <Text style={{ alignSelf: "center", fontWeight: "bold", marginTop: 5 }} >Muhammad Fayyaz</Text>
+        </Content>
+        <Content>
+          <ListItem icon style={styles.barLinkContainer}>
+            <Left>
+              <Button style={{ backgroundColor: "#05CE1D" }}>
+                <Icon active name="home" type="AntDesign"/>
+              </Button>
+            </Left>
+            <Body style={{borderBottomWidth:0,marginLeft:15}}>
+              <Text style={{fontSize:15}}>Home</Text>
+            </Body>
+          </ListItem>
+          <ListItem icon style={styles.barLinkContainer}>
+            <Left>
+              <Button style={{ backgroundColor: "#05CE1D" }}>
+                <Icon active name="home" type="AntDesign"/>
+              </Button>
+            </Left>
+            <Body style={{borderBottomWidth:0,marginLeft:15}}>
+              <Text style={{fontSize:15}}>Home</Text>
+            </Body>
+          </ListItem>
+          <ListItem icon style={styles.barLinkContainer}>
+            <Left>
+              <Button style={{ backgroundColor: "#05CE1D" }}>
+                <Icon active name="home" type="AntDesign"/>
+              </Button>
+            </Left>
+            <Body style={{borderBottomWidth:0,marginLeft:15}}>
+              <Text style={{fontSize:15}}>Home</Text>
+            </Body>
+          </ListItem>
+          <ListItem icon style={styles.barLinkContainer}>
+            <Left>
+              <Button style={{ backgroundColor: "#05CE1D" }}>
+                <Icon active name="home" type="AntDesign"/>
+              </Button>
+            </Left>
+            <Body style={{borderBottomWidth:0,marginLeft:15}}>
+              <Text style={{fontSize:15}}>Home</Text>
+            </Body>
+          </ListItem>
+          <Text style={{borderTopWidth:1,height:0,borderColor:"#bfbfbf"}} ></Text>
+          <ListItem icon style={styles.barLinkContainer}>
+            <Left>
+              <Button style={{ backgroundColor: "#05CE1D" }}>
+                <Icon active name="setting" type="AntDesign"/>
+              </Button>
+            </Left>
+            <Body style={{borderBottomWidth:0,marginLeft:15}}>
+              <Text style={{fontSize:15}}>Settings</Text>
+            </Body>
+          </ListItem>
+          <ListItem icon style={styles.barLinkContainer}>
+            <Left>
+              <Button style={{ backgroundColor: "#05CE1D" }}>
+                <Icon active name="info" type="AntDesign"/>
+              </Button>
+            </Left>
+            <Body style={{borderBottomWidth:0,marginLeft:15}}>
+              <Text style={{fontSize:15}}>About</Text>
+            </Body>
+          </ListItem>
+          <ListItem icon style={styles.barLinkContainer}>
+            <Left>
+              <Button style={{ backgroundColor: "#05CE1D" }}>
+                <Icon active name="feedback" type="MaterialIcons"/>
+              </Button>
+            </Left>
+            <Body style={{borderBottomWidth:0,marginLeft:15}}>
+              <Text style={{fontSize:15}}>Feedback</Text>
+            </Body>
+          </ListItem>
+          <Text style={{borderTopWidth:1,height:0,borderColor:"#bfbfbf"}} ></Text>
+          <ListItem icon style={styles.barLinkContainer}>
+            <Left>
+              <Button style={{ backgroundColor: "#05CE1D" }}>
+                <Icon active name="logout" type="AntDesign"/>
+              </Button>
+            </Left>
+            <Body style={{borderBottomWidth:0,marginLeft:15}}>
+              <Text style={{fontSize:15}}>Logout</Text>
+            </Body>
+          </ListItem>
+        </Content>
+      </Content>
     );
   }
 }
---
+++ Frontend/src/screens/Sidebar/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Dimensions } from "react-native";
+import { Left } from "native-base";
+const { height, width } = Dimensions.get("window");
+
+export const styles = StyleSheet.create({
+    sidebarWrapper:{
+        backgroundColor:"#fff",
+        opacity :1
+    },
+    sideBarTopPanel:{
+        backgroundColor:"#f5f5f5",
+        paddingVertical:12
+    },
+    barLinkContainer:{
+        marginVertical:5,
+        // borderBottomWidth:1
+    }
+});
