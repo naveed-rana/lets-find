1d7c044ebe5eea42b45285f8a9db66029ee4cdf4
naveed-rana <rana.naveed812@gmail.com>
2018-12-26 14:18:23 +0500

    route home sries

--- Backend/app.py
+++ Backend/app.py
@@ -3,9 +3,11 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS, cross_origin
 from bson.json_util import dumps
 from bson import json_util, ObjectId
+from datetime import date
+import pymongo
 
 #Flask App Structure
-app = Flask(__name__, static_folder="./dist", template_folder="./")
+app = Flask(__name__, static_folder="./uploads", template_folder="./")
 app.config.from_object(__name__)
 
 #Cors Setup
@@ -25,6 +27,33 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 def index():
     return "Lets find server, up and running"
 
+
+#Get home stories
+@app.route('/homeStories', methods=['GET'])
+@cross_origin()
+def gethomeStories():
+    status = "not-success"
+    try:
+     missing_persons = mongo.db.missing_persons
+     query = missing_persons.find().sort([("createdat",pymongo.ASCENDING)]).limit(5)
+     output = []
+     for missingPerson in query:
+        id = json.loads(dumps(missingPerson['_id']))
+        output.append({
+            'id':id,
+            'img':missingPerson['img'],
+            'name':missingPerson['name'],
+            'gender':missingPerson['gender'], 
+            'disability':missingPerson['disability'],
+            'description':missingPerson['description'],
+            'status':missingPerson['status'],
+            'age':missingPerson['age'],
+            'createdat':missingPerson['createdat'],
+        })
+     return jsonify({'output':output})
+    except Exception, e:
+     print str(e)
+     return status
 
 #RegisterUser Working
 @app.route('/registeruser', methods=['POST'])
@@ -102,19 +131,27 @@ def logoutUser():
 def registerMissingReq():
     status = "not-success"
     missingPerson = {}
-    try:
-        data = request.get_json(silent=True)
-        data = data["missingperson"]
-        for key in data:
-            missingPerson[key] = data[key]
-        task = mongo.db.missing_persons.insert(missingPerson)
-        print(task)
-        status = "success"
+    if request.files['image']:
+        try:
+            image = request.files["image"]
+            image.save('uploads/' + image.filename)
+            missingPerson['img'] = image.filename
+            missingPerson['name'] = request.form['name']
+            missingPerson['gender'] = request.form['gender']
+            missingPerson['disability'] = request.form['disability']
+            missingPerson['description'] = request.form['description']
+            missingPerson['status'] = request.form['status']
+            missingPerson['age'] = request.form['age']
+            missingPerson['createdat'] = str(date.today())  
+            task = mongo.db.missing_persons.insert(missingPerson)
+            print(task)
+            status = "success"
+            return status
+        except Exception, e:
+            print str(e)
+            return status
+    else:
         return status
-    except Exception, e:
-        print str(e)
-        return status
-
 
 if __name__ == "__main__":
-    app.run(port='8080', debug=True)
+    app.run(host='0.0.0.0',port='2020', debug=True)
