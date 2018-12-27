1091afac941a2887dfb6a6189fa4b72c4b562045
abaidmalik243 <abaidmalik243@gmail.com>
2018-12-27 15:25:53 +0500

    show data on search screen

--- Frontend/src/redux/reducers/MissingPerson/index.js
+++ Frontend/src/redux/reducers/MissingPerson/index.js
@@ -3,20 +3,25 @@ import { ADD_PERSON } from '../../actions/missingPersonAction';
 import intialArray from '../../fakeArray';
 
 const INITIAL_STATE = {
-    homeStories:intialArray
+    homeStories: intialArray
 };
+console.log('Reducer: for all  ', INITIAL_STATE.homeStories)
 
 function AddReducer(state = INITIAL_STATE, action) {
     switch (action.type) {
 
         case ADD_PERSON:
             {
-                var list = state.homeStories;
-                var newList = list.concat([action.payload]);
-                return {
+                // var list = state.homeStories;
+                // console.log('Reducer: before ', state.homeStories)
+                // var homeStories = list.concat([action.payload]);
+                // console.log('Reducer: Middle ', homeStories)
+
+                return { 
                     ...state,
-                    homeStories: newList
+                    homeStories: state.homeStories.concat([action.payload])
                 }
+
             }
 
 
