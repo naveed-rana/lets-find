aec4305ccce9a57417794d888467f19e3dbbbcb7
Asif-chohan <rmasifchohan@gmail.com>
2019-01-02 20:09:58 +0500

    person detail screen modified

--- Frontend/src/screens/PersonelDetails/index.js
+++ Frontend/src/screens/PersonelDetails/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Col, Row, Grid } from "react-native-easy-grid";
-import { StatusBar, Image,Share } from 'react-native';
-import Communications from 'react-native-communications';
+import { StatusBar, Image, Share } from "react-native";
+import Communications from "react-native-communications";
 import {
   View,
   Text,
@@ -19,155 +19,213 @@ import {
   Title,
   Card,
   CardItem
-} from 'native-base';
+} from "native-base";
 
-import styles from './style';
+import styles from "./style";
 
 export default class PersonalDetail extends Component {
-
   render() {
     const { navigation } = this.props;
-    const data = navigation.getParam('data', 'NO-Data');
+    const data = navigation.getParam("data", "NO-Data");
 
     return (
       <Container>
-
-        <Header style={{ backgroundColor: '#05ce1d' }}>
-          <StatusBar
-            backgroundColor="#05ce1d"
-            barStyle="light-content"
-          />
-          <Left><Icon type="AntDesign" name="arrowleft" style={{ color: "#fff", }}
-            onPress={() => navigation.goBack()}
-          /></Left>
+        <Header style={{ backgroundColor: "#05ce1d" }}>
+          <StatusBar backgroundColor="#05ce1d" barStyle="light-content" />
+          <Left>
+            <Icon
+              type="AntDesign"
+              name="arrowleft"
+              style={{ color: "#fff" }}
+              onPress={() => navigation.goBack()}
+            />
+          </Left>
           <Body>
-            <Title>{data.name + ' ' + data.status} Person</Title>
+            <Title>{data.name + " " + data.status} Person</Title>
           </Body>
-
         </Header>
         <Content>
-          <Grid>
-            <Row size={50}>
-              <Col>
-                <View style={styles.imagePadding}>
-                  <Image style={styles.imageStyle} source={require('../../media/personel6.jpg')} />
-                </View>
-              </Col>
-              <Col>
-                <View style={styles.topDetails}>
-                  <Text style={styles.missingPersonTitle}>{data.name}</Text>
+          <View style={styles.imagePadding}>
+            <Image
+              style={styles.imageStyle}
+              source={require("../../media/sham.jpg")}
+            />
+          </View>
 
-                  <View style={styles.PersonalDetailView}>
-                    <Left>
-                      <Text style={styles.topLeftAbout}>Post by</Text>
-                    </Left>
-                    <Right>
-                      <Text style={styles.topLeftAboutDetail}>{data.post_By}</Text>
-                    </Right>
-                  </View>
-                  <View style={styles.PersonalDetailView}>
-                    <Left>
-                      <Text style={styles.topLeftAbout}>Age</Text>
-                    </Left>
-                    <Right>
-                      <Text style={styles.topLeftAboutDetail}>{data.age}</Text>
-                    </Right>
-                  </View>
-                  <View style={styles.PersonalDetailView}>
-                    <Left>
-                      <Text style={styles.topLeftAbout}>Gender</Text>
-                    </Left>
-                    <Right>
-                      <Text style={styles.topLeftAboutDetail}>{data.gender}</Text>
-                    </Right>
-                  </View>
-                  <View style={styles.PersonalDetailView}>
-                    <Left>
-                      <Text style={styles.topLeftAbout}>Status</Text>
-                    </Left>
-                    <Right>
-                      <Text style={styles.topLeftAboutDetail}>{data.status}</Text>
-                    </Right>
-                  </View>
-                  <View style={styles.PersonalDetailView}>
-                    <Left>
-                      <Text style={styles.topLeftAbout}>Disability</Text>
-                    </Left>
-                    <Right>
-                      <Text style={styles.topLeftAboutDetail}>{data.disability}</Text>
-                    </Right>
-                  </View>
-                </View>
-              </Col>
-            </Row>
-            <Row>
-              <View style={styles.bottomStyleView}>
-                <Text style={styles.ShortLocataionText}>Short Description</Text>
-                <Card>
-                  <CardItem>
-                    <Body>
-                      <Text style={styles.descripionText}>{data.description}</Text>
-                    </Body>
-                  </CardItem>
-                </Card>
+          <View style={styles.topDetails}>
+            <View style={styles.PersonalDetailView}>
+              <Left>
+                <Text style={styles.topLeftAbout}>Name</Text>
+              </Left>
+              <Right>
+                <Text style={styles.missingPersonTitle}>{data.name}</Text>
+              </Right>
+            </View>
+            {/* <View style={styles.PersonalDetailView}>
+              <Left>
+                <Text style={styles.topLeftAbout}>Post by</Text>
+              </Left>
+              <Right>
+                <Text style={styles.topLeftAboutDetail}>{data.post_By}</Text>
+              </Right>
+            </View> */}
+            <View style={styles.PersonalDetailView}>
+              <Left>
+                <Text style={styles.topLeftAbout}>Age</Text>
+              </Left>
+              <Right>
+                <Text style={styles.topLeftAboutDetail}>{data.age}</Text>
+              </Right>
+            </View>
+            <View style={styles.PersonalDetailView}>
+              <Left>
+                <Text style={styles.topLeftAbout}>Gender</Text>
+              </Left>
+              <Right>
+                <Text style={styles.topLeftAboutDetail}>{data.gender}</Text>
+              </Right>
+            </View>
+            <View style={styles.PersonalDetailView}>
+              <Left>
+                <Text style={styles.topLeftAbout}>Status</Text>
+              </Left>
+              <Right>
+                <Text style={styles.topLeftAboutDetail}>{data.status}</Text>
+              </Right>
+            </View>
+            <View style={styles.PersonalDetailView}>
+              <Left>
+                <Text style={styles.topLeftAbout}>Disability</Text>
+              </Left>
+              <Right>
+                <Text style={styles.topLeftAboutDetail}>{data.disability}</Text>
+              </Right>
+            </View>
+          </View>
 
+          <View style={styles.bottomStyleView}>
+            <Text style={styles.ShortLocataionText}>Short Description</Text>
+            <Card>
+              <CardItem>
+                <Body>
+                  <Text style={styles.descripionText}>{data.description}</Text>
+                </Body>
+              </CardItem>
+            </Card>
 
-                <View style={{ flexDirection: 'row', marginVertical: 10 }}>
-                  <Left>
-                    <Text style={styles.ShortLocataionText}>Location</Text>
-                  </Left>
-                  <Button full rounded iconLeft success>
-                    <Icon name='map' />
-                    <Text uppercase={false}>Look at the map</Text>
-                  </Button>
-                </View>
+            <View style={{ flexDirection: "row", marginVertical: 10 }}>
+              <Left>
+                <Text style={styles.ShortLocataionText}>Location</Text>
+              </Left>
+              <Button full rounded iconLeft success>
+                <Icon name="map" />
+                <Text uppercase={false}>Look at the map</Text>
+              </Button>
+            </View>
 
+            <Card>
+              <CardItem>
+                <Body>
+                  <Text style={styles.descripionText}>{data.location}</Text>
+                </Body>
+              </CardItem>
+            </Card>
+          </View>
 
-                <Card>
-                  <CardItem>
-                    <Body>
-                      <Text style={styles.descripionText}>{data.location}</Text>
-                    </Body>
-                  </CardItem>
-                </Card>
+            <View
+              style={{
+                borderBottomColor: "black",
+                borderBottomWidth: 2,
+                width:"100%",
+                marginVertical: 10
+              }}
+            />
 
-                <View style={{ flexDirection: 'row', marginTop: 10 }}>
-                  <Left>
-                    <Button style={styles.btnBorder} iconLeft success onPress={() => Communications.phonecall(`${data.mobile}`, true)}>
-                      <Icon type="FontAwesome" name='phone' />
-                      <Text uppercase={true} style={{ fontSize: 16 }} >Call</Text>
-                    </Button>
-                  </Left>
-                  <Button style={styles.btnBorder} iconLeft success onPress={() => Communications.text(`${data.mobile}`)}>
-                    <Icon name='sms' type="MaterialIcons" />
-                    <Text uppercase={true} style={{ fontSize: 16 }} >SMS</Text>
-                  </Button>
-                </View>
-                <View>
+            <View style={styles.PersonalDetailView}>
+              <Left>
+                <Text style={styles.topLeftAbout}>Posted By</Text>
+              </Left>
+              <Right>
+                <Text style={styles.topLeftAboutDetail}>{data.disability}</Text>
+              </Right>
+            </View>
+            <View style={styles.PersonalDetailView}>
+              <Left>
+                <Text style={styles.topLeftAbout}>Contect</Text>
+              </Left>
+              <Right>
+                <Text style={styles.topLeftAboutDetail}>{data.disability}</Text>
+              </Right>
+            </View>
+
+            <View style={{ flexDirection: "row", marginTop: 10 }}>
+              
+              
+              
+              
+              <Left>
                 <Button
-                style={{borderRadius: 5,marginTop:15}}
-                full success onPress={()=>{
-                              Share.share({
-                                message: `*Missing Person Alert* \n Name: *${data.name}* \n Age: *${data.age}* \n Gender: *${data.gender}* \n Disability: *${data.disability}* \n Location: *${data.location}* \n Contact No.: *${data.mobile}*`,
-                                url:'http://img.gemejo.com/product/8c/099/cf53b3a6008136ef0882197d5f5.jpg',
-                                title: 'Wow, did you see that?'
-                              })
-                            }}>
-                <Icon 
-                style={{ color:'white',}}
-                type="AntDesign"
-                name="sharealt"
-                            />
-                    <Text uppercase={true} style={{ fontSize: 16 }} >share</Text>
-                  </Button>
-                </View>
+                  style={styles.btnBorder}
+                  iconLeft
+                  success
+                  onPress={() =>
+                    Communications.phonecall(`${data.mobile}`, true)
+                  }
+                >
+                  <Icon type="FontAwesome" name="phone" />
+                  <Text uppercase={true} style={{ fontSize: 16 }}>
+                    Call
+                  </Text>
+                </Button>
+              </Left>
+              <Button
+                style={styles.btnBorder}
+                iconLeft
+                success
+                onPress={() => Communications.text(`${data.mobile}`)}
+              >
+                <Icon name="sms" type="MaterialIcons" />
+                <Text uppercase={true} style={{ fontSize: 16 }}>
+                  SMS
+                </Text>
+              </Button>
               </View>
-            </Row>
-          </Grid>
-        </Content>
+            <View>
+            
 
+
+
+              <Button
+                style={{ borderRadius: 5, marginTop: 15 }}
+                full
+                success
+                onPress={() => {
+                  Share.share({
+                    message: `*Missing Person Alert* \n Name: *${
+                      data.name
+                    }* \n Age: *${data.age}* \n Gender: *${
+                      data.gender
+                    }* \n Disability: *${data.disability}* \n Location: *${
+                      data.location
+                    }* \n Contact No.: *${data.mobile}*`,
+                    url:
+                      "http://img.gemejo.com/product/8c/099/cf53b3a6008136ef0882197d5f5.jpg",
+                    title: "Wow, did you see that?"
+                  });
+                }}
+              >
+                <Icon
+                  style={{ color: "white" }}
+                  type="AntDesign"
+                  name="sharealt"
+                />
+                <Text uppercase={true} style={{ fontSize: 16 }}>
+                  share
+                </Text>
+              </Button>
+            </View>
+        </Content>
       </Container>
     );
   }
 }
-
--- Frontend/src/screens/PersonelDetails/style.js
+++ Frontend/src/screens/PersonelDetails/style.js
@@ -1,50 +1,64 @@
-export default styles = {
-    btnBorder:{
-      borderRadius:5,
-      paddingLeft:15,
-      paddingRight:15
-    },
-    descripionText:{
-      fontSize:14
-    },
-    ShortLocataionText:{ 
-      fontSize: 15, 
-      fontWeight: 'bold' 
-    },
-    bottomStyleView:{ 
-      padding: 20, 
-      width: '100%' 
-    },
-    topLeftAboutDetail:{ 
-      fontSize: 14, 
-      fontWeight: 'bold' 
-    },
-    topLeftAbout:{ 
-      fontSize: 15, 
-      color: '#05ce1d' 
-    },
-    PersonalDetailView:{ 
-      flexDirection: 'row', 
-      width: '100%' ,
-      marginBottom:5
-    },
-    missingPersonTitle:{ 
-      textAlign: 'left', 
-      fontSize: 17,
-      fontWeight:'bold' ,
-      marginBottom:10
-    },
-    topDetails:{ 
-      paddingHorizontal: 5, 
-      paddingVertical: 20, 
-      paddingRight: 20 
-    },
-    imageStyle:{ 
-      width: 150, 
-      height: 150,
-      borderRadius:5 
-    },
-    imagePadding:{ 
-      padding: 20 
-    }
+import { Dimensions } from "react-native";
+const dimensions = Dimensions.get("window");
+const imageHeight = Math.round((dimensions.width * 9) / 16);
+const imageWidth = dimensions.width;
+
+export default (styles = {
+  btnBorder: {
+    borderRadius: 5,
+    paddingLeft: 15,
+    paddingRight: 15
+  },
+  descripionText: {
+    fontSize: 14
+  },
+  ShortLocataionText: {
+    fontSize: 15,
+    
+    color: "#05ce1d"
+  },
+  bottomStyleView: {
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    width: "100%"
+  },
+  topLeftAboutDetail: {
+    fontSize: 14,
+    fontWeight: "bold"
+  },
+  topLeftAbout: {
+    fontSize: 15,
+    color: "#05ce1d"
+  },
+  PersonalDetailView: {
+    flexDirection: "row",
+    width: "100%",
+    marginBottom: 5,
+    
+  },
+  missingPersonTitle: {
+    textAlign: "left",
+    fontSize: 17,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  topDetails: {
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+  },
+  imageStyle: {
+    // borderRadius:5 ,
+    // resizeMode : 'contain',
+    // height: imageHeight,
+    // width: imageWidth,
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  imagePadding: {
+    // padding: 20 ,
+    height: 190,
+    width: "100%",
+    backgroundColor: "red"
   }
+});
