fc672d2cf449247adf913054bf85fee0685965f2
FayyazGM <fayyazjee11@gmail.com>
2019-01-04 00:13:39 +0500

    search screen with top header and image modal also done in search image

--- Frontend/src/screens/searchScreen/index.js
+++ Frontend/src/screens/searchScreen/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Image, StatusBar, ScrollView, TouchableOpacity } from 'react-native';
+import { Image, StatusBar, ScrollView, TouchableOpacity, Modal } from 'react-native';
 import { View, Text, Content, Item, Input, Icon, Card, CardItem, Body, Container, Picker, Form, Button } from 'native-base';
 import ImagePicker from "react-native-image-picker";
+import ImageViewer from "react-native-image-zoom-viewer";
 import styles from './style';
 
 import { connect } from 'react-redux';
@@ -9,10 +10,10 @@ import { connect } from 'react-redux';
 const options = {
     title: "Select Option",
     storageOptions: {
-      skipBackup: true,
-      path: "images"
+        skipBackup: true,
+        path: "images"
     }
-  };
+};
 class SearchScreen extends Component {
     constructor(props) {
         super(props);
@@ -37,27 +38,28 @@ class SearchScreen extends Component {
                 mobile: '+92 303 4766669',
                 post_By: 'Asif'
             }],
+            modalVisible: false
         };
     }
     uploadImage = () => {
         ImagePicker.showImagePicker(options, response => {
-          console.log("Response = ", response);
-    
-          if (response.didCancel) {
-            console.log("User cancelled image picker");
-          } else if (response.error) {
-            console.log("ImagePicker Error: ", response.error);
-          } else if (response.customButton) {
-            console.log("User tapped custom button: ", response.customButton);
-          } else {
-            const source = { uri: response.uri };
-    
-            this.setState({
-              image: source
-            });
-          }
+            console.log("Response = ", response);
+
+            if (response.didCancel) {
+                console.log("User cancelled image picker");
+            } else if (response.error) {
+                console.log("ImagePicker Error: ", response.error);
+            } else if (response.customButton) {
+                console.log("User tapped custom button: ", response.customButton);
+            } else {
+                const source = { uri: response.uri };
+
+                this.setState({
+                    image: source
+                });
+            }
         });
-      };
+    };
     componentDidMount() {
         this.setState({ fakeArray: this.props.missingPersons })
     }
@@ -115,7 +117,15 @@ class SearchScreen extends Component {
 
                     <Item style={styles.itemStyle} >
                         <View style={styles.searchInput}>
-                            <Input placeholder='Search' />
+                            <Input placeholder='Search By Name' />
+                            <TouchableOpacity style={styles.cameraIconBtn}>
+                                <Icon style={styles.camIcon} type="AntDesign" name="search1" />
+                            </TouchableOpacity>
+                        </View>
+                    </Item>
+                    <Item style={styles.itemStyle1} >
+                        <View style={styles.searchInput}>
+                            <Input placeholder='Search By Image' readOnly="" />
                             <TouchableOpacity style={styles.cameraIconBtn} onPress={this.uploadImage}>
                                 <Icon style={styles.camIcon} type="Entypo" name="camera" />
                             </TouchableOpacity>
@@ -214,94 +224,128 @@ class SearchScreen extends Component {
                                         <Body>
                                             <View style={styles.cardInnerContainer}>
                                                 <View>
-                                                    <Image
-                                                        style={styles.filterImage}
-                                                        source={require("../../media/sham.jpg")}
-                                                    />
+                                                    <Modal
+                                                        visible={this.state.modalVisible}
+                                                        transparent={true}
+                                                        animationType="slide"
+                                                        // transparent={false}
+                                                        onRequestClose={() => {
+                                                            this.modalVisible(false);
+                                                        }}
+                                                    >
+                                                        <View style={styles.modalOverlay}>
+                                                            <Icon
+                                                                style={styles.modalClose}
+                                                                type="AntDesign"
+                                                                name="close"
+                                                                onPress={() =>
+                                                                    this.setState({ modalVisible: false })
+                                                                }
+                                                            />
+                                                            <View
+                                                                style={{ flex: 1, justifyContent: "center" }}
+                                                            >
+                                                                <Image
+                                                                    style={styles.modalImage}
+                                                                    source={require("../../media/sham.jpg")}
+                                                                />
+                                                            </View>
+                                                        </View>
+                                                    </Modal>
+                                                    <TouchableOpacity
+                                                        onPress={() =>
+                                                            this.setState({ modalVisible: true })
+                                                        }
+                                                    >
+                                                        <Image
+                                                            style={styles.filterImage}
+                                                            source={require("../../media/sham.jpg")}
+                                                        />
+                                                    </TouchableOpacity>
                                                 </View>
 
-                                                <View style={styles.textContainer}>
-                                                    <View style={styles.cardHeader}>
-                                                        <Text>{data.name}</Text>
+                                                    <View style={styles.textContainer}>
+                                                        <View style={styles.cardHeader}>
+                                                            <Text>{data.name}</Text>
 
-                                                        <Text style={styles.statusText}>{data.status}</Text>
-                                                    </View>
+                                                            <Text style={styles.statusText}>{data.status}</Text>
+                                                        </View>
 
-                                                    <View>
-                                                        <Text style={styles.nameText}>
-                                                            Posted By {data.post_By}
-                                                        </Text>
-                                                    </View>
+                                                        <View>
+                                                            <Text style={styles.nameText}>
+                                                                Posted By {data.post_By}
+                                                            </Text>
+                                                        </View>
 
-                                                    <View style={{ flexDirection: "row", paddingTop: 5 }}>
-                                                        <Icon
-                                                            style={{ marginLeft: -5 }}
-                                                            type="EvilIcons"
-                                                            name="location"
-                                                        />
-                                                        <Text style={{ fontSize: 13 }}>
-                                                            {data.location}
-                                                        </Text>
-                                                    </View>
+                                                        <View style={{ flexDirection: "row", paddingTop: 5 }}>
+                                                            <Icon
+                                                                style={{ marginLeft: -5 }}
+                                                                type="EvilIcons"
+                                                                name="location"
+                                                            />
+                                                            <Text style={{ fontSize: 13 }}>
+                                                                {data.location}
+                                                            </Text>
+                                                        </View>
 
-                                                    <View style={styles.cardHeader}>
-                                                        <Text
-                                                            style={styles.readMore}
-                                                            onPress={() =>
-                                                                this.props.navigation.navigate("PersonDetail", {
-                                                                    data: {
-                                                                        id: data.id,
-                                                                        name: data.name,
-                                                                        status: data.status,
-                                                                        post_By: data.post_By,
-                                                                        age: data.age,
-                                                                        gender: data.gender,
-                                                                        disability: data.disability,
-                                                                        description: data.description,
-                                                                        location: data.location,
-                                                                        mobile: data.mobile
-                                                                    }
-                                                                })
-                                                            }
-                                                        >
-                                                            Read More
+                                                        <View style={styles.cardHeader}>
+                                                            <Text
+                                                                style={styles.readMore}
+                                                                onPress={() =>
+                                                                    this.props.navigation.navigate("PersonDetail", {
+                                                                        data: {
+                                                                            id: data.id,
+                                                                            name: data.name,
+                                                                            status: data.status,
+                                                                            post_By: data.post_By,
+                                                                            age: data.age,
+                                                                            gender: data.gender,
+                                                                            disability: data.disability,
+                                                                            description: data.description,
+                                                                            location: data.location,
+                                                                            mobile: data.mobile
+                                                                        }
+                                                                    })
+                                                                }
+                                                            >
+                                                                Read More
                             </Text>
 
-                                                        <Icon
-                                                            onPress={() => {
-                                                                Share.share({
-                                                                    message: `*Missing Person Alert* \n Name: *${
-                                                                        data.name
-                                                                        }* \n Age: *${data.age}* \n Gender: *${
-                                                                        data.gender
-                                                                        }* \n Disability: *${
-                                                                        data.disability
-                                                                        }* \n Location: *${
-                                                                        data.location
-                                                                        }* \n Contact No.: *${data.mobile}*`,
-                                                                    url:
-                                                                        "http://img.gemejo.com/product/8c/099/cf53b3a6008136ef0882197d5f5.jpg",
-                                                                    title: "Wow, did you see that?"
-                                                                });
-                                                            }}
-                                                            style={{
-                                                                marginTop: -5,
-                                                                fontSize: 25,
-                                                                color: "gray"
-                                                            }}
-                                                            type="AntDesign"
-                                                            name="sharealt"
-                                                        />
+                                                            <Icon
+                                                                onPress={() => {
+                                                                    Share.share({
+                                                                        message: `*Missing Person Alert* \n Name: *${
+                                                                            data.name
+                                                                            }* \n Age: *${data.age}* \n Gender: *${
+                                                                            data.gender
+                                                                            }* \n Disability: *${
+                                                                            data.disability
+                                                                            }* \n Location: *${
+                                                                            data.location
+                                                                            }* \n Contact No.: *${data.mobile}*`,
+                                                                        url:
+                                                                            "http://img.gemejo.com/product/8c/099/cf53b3a6008136ef0882197d5f5.jpg",
+                                                                        title: "Wow, did you see that?"
+                                                                    });
+                                                                }}
+                                                                style={{
+                                                                    marginTop: -5,
+                                                                    fontSize: 25,
+                                                                    color: "gray"
+                                                                }}
+                                                                type="AntDesign"
+                                                                name="sharealt"
+                                                            />
+                                                        </View>
                                                     </View>
                                                 </View>
-                                            </View>
                                         </Body>
                                     </CardItem>
                                 </Card>
                             </View>
-                        );
-                    })}
-
+                                );
+                            })}
+        
                     {/* <View style={styles.cardContainer}>
                         <Card>
                             <CardItem>
@@ -360,20 +404,20 @@ class SearchScreen extends Component {
 
                 </ScrollView>
 
-                {/* end card 2 */}
+                {/* end card 2 */ }
 
             </Container>
-
-
-        );
-    }
-}
-
+        
+        
+                );
+            }
+        }
+        
 const mapStateToProps = (state) => {
 
     return {
-        missingPersons: state.misingPersons.homeStories
-    }
-}
-
+                        missingPersons: state.misingPersons.homeStories
+                }
+            }
+            
 export default connect(mapStateToProps, null)(SearchScreen);
